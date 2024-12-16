63809765534e88ff9bdebd16deca1c717d7c4150
thomasavguld <rosen.thomas@gmail.com>
2024-12-16 14:01:30 +0100

    nånting

--- helloworld.py
+++ helloworld.py
@@ -1,8 +1,1 @@
-print ("hello world!")
-print ("Jag är en släthjärnad sälkut")
-#Git intro
-
-```bash
-git config --global user.name "thomasavguld"
-git config --global user.email "rosen.thomas@gmail.com"
-```
+print ("hello world! Var ligger närmaste Västerbro, tro?")

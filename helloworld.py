aaf60ffdfbbfb5d5a31e151a68f8f8602f56ad86
thomasavguld <rosen.thomas@gmail.com>
2024-12-16 13:11:44 +0100

    commit one

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+print ("hello world!")
+print ("Jag är en släthjärnad sälkut")
+#Git intro
+
+```bash
+git config --global user.name "thomasavguld"
+git config --global user.email "rosen.thomas@gmail.com"
+```
